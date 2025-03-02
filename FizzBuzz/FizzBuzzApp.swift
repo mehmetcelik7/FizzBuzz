8d596203fc4ecc22d66756f55496b8066b43fdcf
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-02 20:09:01 +0300

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by mehmet Çelik on 2.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
